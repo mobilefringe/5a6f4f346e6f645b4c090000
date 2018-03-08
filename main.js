8f4073d98413da266d4af878b1c6e658a039a32b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-08 11:29:48 -0500

    Mapplic for Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -44,9 +44,9 @@ require.config({
         // 'mm_mapsvg': 'https://mmvue.codecloudapp.com/mallmaverick_svgmap',
         // 'svg-map' : 'https://mmvue.codecloudapp.com/svg_map.vue?noext',
         'mousewheel' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min',
-        'mapplic' : 'https://mmvue.codecloudapp.com/mapplic',
+        'mapplic' : 'https://preview-mmvue.codecloudapp.com/mapplic',
         'hammer' : 'https://codecloud.cdn.speedyrails.net/sites/59bac7db6e6f644f22ba0000/text/javascript/1484859750000/hammer.min',
-        'mapplic-map' : 'https://mmvue.codecloudapp.com/mapplic.vue?noext'
+        'mapplic-map' : 'https://preview-mmvue.codecloudapp.com/mapplic.vue?noext'
     }
 });
 
