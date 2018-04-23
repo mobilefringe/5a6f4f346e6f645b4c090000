b31ac4861f8f9b66ffeacc4b368e33ec6f232988
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-23 14:03:13 -0400

    Mapplic for Northpark 1.0  /main.js

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
         },
         created() {
             this.loadData().then(response => {
-                this.dataLoaded = true;      
+                this.dataLoaded = false;      
             });
         },
         watch: {
